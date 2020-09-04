fb0033c8ed6ceab7effbdac1bded54df968f8d21
Brenson <brensonw300@gmail.com>
2020-09-04 08:23:44 -0700

    Went into tabs.js and wrote out code

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,40 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+
+const allTopics = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((resolve) => {
+    resolve.data.topics.forEach(data => {
+    const tabs = tab(data)
+    allTopics.appendChild(tabs);
+    })   
+    // console.log('resolve',resolve)
+});
+
+function tab(topic){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    allTopics.appendChild(tab);
+    tab.dataset.tab = topic;
+    // console.log(tab)
+    tab.addEventListener('click', () => {
+        const card = document.querySelectorAll('.card'); 
+        card.forEach((cards) => {
+            cards.style.display = 'none'
+            if(cards.dataset.tab == tab.dataset.tab){
+                cards.style.display = 'flex'
+            }
+        })
+        const allTabs = document.querySelectorAll('.tab');
+        allTabs.forEach((tabs) => {
+            tabs.classList.remove('active-tab');
+            tab.classList.add('active-tab');
+        })
+    })
+        return tab;
+} 
